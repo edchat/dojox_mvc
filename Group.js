f21b6e063c07f033a2c7596a13a7b78bbe677fa8
Ed Chatelain <ed.chatelain@gmail.com>
2012-01-20 13:52:55 -0500

    two possible fixes for 14423 to look at and discuss

--- Group.js
+++ Group.js
@@ -1,4 +1,4 @@
-define(["dojo/_base/declare", "dijit/_WidgetBase"], function(declare, WidgetBase){
+define(["dojo/_base/declare", "dijit/_WidgetBase", 	"dojo/_base/lang"], function(declare, WidgetBase, lang){
 	/*=====
 		WidgetBase = dijit._WidgetBase;
 		declare = dojo.declare;
@@ -12,5 +12,19 @@ define(["dojo/_base/declare", "dijit/_WidgetBase"], function(declare, WidgetBase
 		//		A group is usually bound to an intermediate dojo.Stateful node in the data model.
 		//		Child dijits or custom view components inside a group inherit their parent
 		//		data binding context from it.
+		startup: function(){
+			// This code needed for ticket 14423 is using removeRepeatNode on a repeat to work with mobile.lists
+			// this.select and this.onCheckStateChanged are called by ListItem so they need to be set
+			// but it seems like a bit of a hack.
+			var parent = null;
+			if(lang.isFunction(this.getParent)){
+				if(this.getParent() && this.getParent().removeRepeatNode){
+					this.select = this.getParent().select;
+					this.onCheckStateChanged = this.getParent().onCheckStateChanged;
+				}
+			}			
+			this.inherited(arguments);			
+		},
+		
 	});
 });

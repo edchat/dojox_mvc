10f9a1a8c0271ae48fcd8996d533156101d1835e
asudoh <asudoh@gmail.com>
2012-02-28 18:07:58 +0900

    Add examples to some components.

--- ListController.js
+++ ListController.js
@@ -90,6 +90,28 @@ define([
 		// summary:
 		//		A controller working with array model, managing its cursor.
 		//		NOTE - If this class is used with a widget by data-dojo-mixins, make sure putting the widget in data-dojo-type and putting this class to data-dojo-mixins.
+		// example:
+		//		The text box changes its value every two seconds.
+		// |		<html>
+		// |			<head>
+		// |				<script src="/path/to/dojo-toolkit/dojo/dojo.js" type="text/javascript" data-dojo-config="parseOnLoad: 0"></script>
+		// |				<script type="text/javascript">
+		// |					require([
+		// |						"dojo/parser", "dijit/registry", "dojox/mvc/StatefulArray",
+		// |						"dijit/form/TextBox", "dojox/mvc/at", "dojox/mvc/ListController", "dojo/domReady!"
+		// |					], function(parser, registry, StatefulArray){
+		// |						var count = 0;
+		// |						model = new StatefulArray([{value: "First"}, {value: "Second"}, {value: "Third"}, {value: "Fourth"}, {value: "Fifth"}]);
+		// |						setInterval(function(){ registry.byId("ctrl").set("cursorIndex", ++count % 5); }, 2000);
+		// |						parser.parse();
+		// |					});
+		// |				</script>
+		// |			</head>
+		// |			<body>
+		// |				<span id="ctrl" data-dojo-type="dojox.mvc.ListController" data-dojo-props="model: model"></span>
+		// |				<input type="text" data-dojo-type="dijit.form.TextBox" data-dojo-props="value: dojox.mvc.at('widget:ctrl', 'value')">
+		// |			</body>
+		// |		</html>
 
 		// idProperty: String
 		//		The property name in element in the model array, that works as its identifier.

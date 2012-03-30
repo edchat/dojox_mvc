3014e5d5dddd0ca4d280d98aa572b328ec0bce71
asudoh <asudoh@gmail.com>
2012-03-30 16:24:17 +0900

    Support for new attribute for data binding.

--- ParserExtension.js
+++ ParserExtension.js
@@ -6,9 +6,10 @@ define([
 	"dojo/has",
 	"dojo/has!dojo-mobile-parser?:dojo/parser",
 	"dojo/has!dojo-parser?:dojox/mobile/parser",
+	"dojox/mvc/_atBindingMixin",
 	"dojox/mvc/Element",
 	"dojox/mvc/FormElement"
-], function(require, kernel, lang, win, has, parser, mobileParser){
+], function(require, kernel, lang, win, has, parser, mobileParser, _atBindingMixin){
 
 	// module:
 	//		dojox/mvc/ParserExtension
@@ -19,34 +20,8 @@ define([
 	try{ has.add("dojo-parser", !!require("dojo/parser"));  }catch(e){}
 	try{ has.add("dojo-mobile-parser", !!require("dojox/mobile/parser")); }catch(e){}
 
-	function _eval(/*String*/ s){
-		return eval("(" + s + ")");
-	}
-
 	if(has("dojo-parser")){
-		var oldConstruct = parser.construct, oldScan = parser.scan;
-
-		parser.construct = /*====== dojo.parser.construct = ======*/ function(/*Function*/ ctor, /*DOMNode*/ node, /*Object?*/ mixin, /*Object?*/ options, /*DOMNode[]?*/ scripts, /*Object?*/ inherited){
-			// summary:
-			//		TODOC
-
-			var attrData = "data-" + (options.scope || kernel._scopeName) + "-",	// typically "data-dojo-"
-			 dataDojoBind = attrData + "bind",										// typically "data-dojo-bind"
-			 bind = node.getAttribute(dataDojoBind),
-			 props = lang.mixin({}, mixin);
-
-			// Mix things found in data-dojo-props into the params, overriding any direct settings
-			if(bind){
-				try{
-					props.refs = _eval.call(options.propsThis, "{" + bind + "}");
-				}catch(e){
-					// give the user a pointer to their invalid parameters. FIXME: can we kill this in production?
-					throw new Error(e.toString() + " in data-dojo-bind='" + bind + "'");
-				}
-			}
-
-			return oldConstruct.call(this, ctor, node, props, options, scripts, inherited);
-		};
+		var oldScan = parser.scan;
 
 		parser.scan = /*====== dojo.parser.scan = ======*/ function(/*DOMNode?*/ root, /*Object*/ options){
 			// summary:
@@ -55,12 +30,11 @@ define([
 			var list = oldScan.apply(this, lang._toArray(arguments)),
 			 dojoType = (options.scope || kernel._scopeName) + "Type",			// typically "dojoType"
 			 attrData = "data-" + (options.scope || kernel._scopeName) + "-",	// typically "data-dojo-"
-			 dataDojoType = attrData + "type",									// typically "data-dojo-type"
-			 dataDojoBind = attrData + "bind";									// typically "data-dojo-bind"
+			 dataDojoType = attrData + "type";									// typically "data-dojo-type"
 
-			for(var nodes = has("dom-qsa") ? root.querySelectorAll("[" + dataDojoBind + "]") : root.getElementsByTagName("*"), i = 0, l = nodes.length; i < l; i++){
+			for(var nodes = has("dom-qsa") ? root.querySelectorAll("[" + _atBindingMixin.prototype.dataBindAttr + "]") : root.getElementsByTagName("*"), i = 0, l = nodes.length; i < l; i++){
 				var node = nodes[i], foundBindingInAttribs = false;
-				if(!node.getAttribute(dataDojoType) && !node.getAttribute(dojoType) && node.getAttribute(dataDojoBind)){
+				if(!node.getAttribute(dataDojoType) && !node.getAttribute(dojoType) && node.getAttribute(_atBindingMixin.prototype.dataBindAttr)){
 					list.push({
 						"type": /^select|input|textarea$/i.test(node.tagName) ? "dojox/mvc/FormElement" : "dojox/mvc/Element",
 						node: node
@@ -81,20 +55,13 @@ define([
 
 			var dojoType = ((options || {}).scope || kernel._scopeName) + "Type",		// typically "dojoType"
 			 attrData = "data-" + ((options || {}).scope || kernel._scopeName) + "-",	// typically "data-dojo-"
-			 dataDojoType = attrData + "type",											// typically "data-dojo-type"
-			 dataDojoBind = attrData + "bind",											// typically "data-dojo-bind"
-			 dataDojoProps = attrData + "props",										// typically "data-dojo-props"
-			 nodes = has("dom-qsa") ? (root || win.body()).querySelectorAll("[" + dataDojoBind + "]") : (root || win.body()).getElementsByTagName("*");
+			 dataDojoType = attrData + "type";											// typically "data-dojo-type"
+			 nodes = has("dom-qsa") ? (root || win.body()).querySelectorAll("[" + _atBindingMixin.prototype.dataBindAttr + "]") : (root || win.body()).getElementsByTagName("*");
 
 			for(var i = 0, l = nodes.length; i < l; i++){
 				var node = nodes[i], foundBindingInAttribs = false, bindingsInAttribs = [];
-				if(!node.getAttribute(dataDojoType) && !node.getAttribute(dojoType) && node.getAttribute(dataDojoBind)){
+				if(!node.getAttribute(dataDojoType) && !node.getAttribute(dojoType) && node.getAttribute(_atBindingMixin.prototype.dataBindAttr)){
 					node.setAttribute(dataDojoType, /^select|input|textarea$/i.test(node.tagName) ? "dojox/mvc/FormElement" : "dojox/mvc/Element");
-				}
-				var bind = node.getAttribute(dataDojoBind);
-				if(bind){
-					var props = node.getAttribute(dataDojoProps); 
-					node.setAttribute(dataDojoProps, (props ? [props] : []).concat(bind ? [bind] : []).join(","));
 				}
 			}
 

e769a99662212edcbf5eba4016aa472492a2467b
asudoh <asudoh@gmail.com>
2012-05-23 17:42:48 +0900

    Fix for a minor error found as writing docs.

--- equals.js
+++ equals.js
@@ -14,7 +14,7 @@ define([
 			// v: Anything
 			//		The value.
 
-			return lang.isArray(v) ? "array" : lang.isFunction((v || {}).getTime) ? "date" : v != null && {}.toString.call(v) == "[object Object]" ? "object" : "value";
+			return lang.isArray(v) ? "array" : lang.isFunction((v || {}).getTime) ? "date" : v != null && ({}.toString.call(v) == "[object Object]" || lang.isFunction((v || {}).set) && lang.isFunction((v || {}).watch)) ? "object" : "value";
 		},
 
 		equalsArray: function(/*Anything[]*/ dst, /*Anything[]*/ src){

0b45436d07ca05230b8d7e274c6041c91a188cda
asudoh <asudoh@gmail.com>
2012-10-20 05:15:03 +0900

    Fix test case failure in IE.

--- tests/ModelRefController.js
+++ tests/ModelRefController.js
@@ -1,20 +1,28 @@
 define([
 	"doh",
 	"dojo/_base/declare",
+	"dojo/_base/lang",
 	"dojo/Stateful",
 	"dijit/_WidgetBase",
 	"dijit/_TemplatedMixin",
 	"dijit/_WidgetsInTemplateMixin",
 	"../ModelRefController",
 	"dojo/text!./templates/ModelRefControllerInTemplate.html"
-], function(doh, declare, Stateful, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, ModelRefController, template){
+], function(doh, declare, lang, Stateful, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, ModelRefController, template){
 	doh.register("dojox.mvc.tests.ModelRefController", [
-		function attachPoint(){
-			var w = new (declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
-				templateString: template
-			}))({}, document.createElement("div"));
-			w.startup();
-			doh.t(w.controllerNode, "The controllerNode exists in the template widget");
+		{
+			name: "attachPoint",
+			runTest: function(){
+				lang.setObject("model", new Stateful());
+				var w = new (declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+					templateString: template
+				}))();
+				w.startup();
+				doh.t(w.controllerNode, "The controllerNode exists in the template widget");
+			},
+			tearDown: function(){
+				lang.setObject("model", void 0);
+			}
 		}
 	]);
 });
